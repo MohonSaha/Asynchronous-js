091ab16b16e6031f200c86496e46ec7a14c90d69
MohonSaha <practicewithmohon@gmail.com>
2023-03-07 12:39:11 +0600

    js intervals

--- intervals.js
+++ intervals.js
@@ -1,3 +1,4 @@
+// Example of setIntervals for looping data
 
 let num = 0;
 const intervalId = setInterval(()=>{
@@ -7,3 +8,16 @@ const intervalId = setInterval(()=>{
         clearInterval(intervalId)
     }
 }, 500);
+
+
+
+// Example of setIntervals: 
+
+console.log(1);
+console.log(2);
+// console.log(3);
+setInterval(() =>{
+    console.log(3);
+}, 1000)
+console.log(4);
+console.log(5);
